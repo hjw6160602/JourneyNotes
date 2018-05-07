b729f63e0805bb0303f319894ac3fb097b42a5e3
hejiawei <hejiawei@lvmama.com>
2018-05-07 17:46:00 +0800

    活久见的一波carthage update

--- Carthage/Checkouts/Kingfisher/Tests/KingfisherTests/KingfisherManagerTests.swift
+++ Carthage/Checkouts/Kingfisher/Tests/KingfisherTests/KingfisherManagerTests.swift
@@ -4,7 +4,7 @@
 //
 //  Created by Wei Wang on 15/10/22.
 //
-//  Copyright (c) 2017 Wei Wang <onevcat@gmail.com>
+//  Copyright (c) 2018 Wei Wang <onevcat@gmail.com>
 //
 //  Permission is hereby granted, free of charge, to any person obtaining a copy
 //  of this software and associated documentation files (the "Software"), to deal
@@ -366,7 +366,7 @@ class KingfisherManagerTests: XCTestCase {
         let expectation = self.expectation(description: "waiting for downloading finished")
         
         let URLString = testKeys[0]
-        manager.cache.store(testImage, original: testImageData! as Data,
+        manager.cache.store(testImage, original: testImageData as Data,
                             forKey: URLString, processorIdentifier: DefaultImageProcessor.default.identifier,
                             cacheSerializer: DefaultCacheSerializer.default, toDisk: true)
         {
@@ -431,7 +431,7 @@ class KingfisherManagerTests: XCTestCase {
         // Clear original cache first.
         originalCache.clearMemoryCache()
         originalCache.clearDiskCache {
-            originalCache.store(testImage, original: testImageData! as Data,
+            originalCache.store(testImage, original: testImageData as Data,
                                 forKey: URLString, processorIdentifier: DefaultImageProcessor.default.identifier,
                                 cacheSerializer: DefaultCacheSerializer.default, toDisk: true)
             {
@@ -515,6 +515,108 @@ class KingfisherManagerTests: XCTestCase {
         }
         waitForExpectations(timeout: 5, handler: nil)
     }
+
+    func testShouldDownloadAndCacheProcessedImage() {
+        let expectation = self.expectation(description: "waiting for downloading and cache")
+
+        let URLString = testKeys[0]
+        _ = stubRequest("GET", URLString).andReturn(200)?.withBody(testImageData)
+
+        let url = URL(string: URLString)!
+
+        let size = CGSize(width: 1, height: 1)
+        let processor = ResizingImageProcessor(referenceSize: size)
+
+        manager.retrieveImage(with: url, options: [.processor(processor)], progressBlock: nil) {
+            image, _, type, _ in
+            // Can download and cache normally
+            XCTAssertNotNil(image)
+            XCTAssertEqual(image!.size, size)
+            XCTAssertEqual(type, .none)
+
+            self.manager.cache.clearMemoryCache()
+            XCTAssertEqual(self.manager.cache.imageCachedType(forKey: URLString, processorIdentifier: processor.identifier), .disk)
+
+            self.manager.retrieveImage(with: url, options: [.processor(processor)], progressBlock: nil) {
+                image, _, type, _ in
+                XCTAssertNotNil(image)
+                XCTAssertEqual(image!.size, size)
+                XCTAssertEqual(type, .disk)
+
+                expectation.fulfill()
+            }
+        }
+        waitForExpectations(timeout: 5, handler: nil)
+    }
+
+#if os(iOS) || os(tvOS) || os(watchOS)
+    func testShouldApplyImageModifierWhenDownload() {
+        let expectation = self.expectation(description: "waiting for downloading and cache")
+
+        let URLString = testKeys[0]
+        _ = stubRequest("GET", URLString).andReturn(200)?.withBody(testImageData)
+        let url = URL(string: URLString)!
+
+        var modifierCalled = false
+        let modifier = AnyImageModifier { image in
+            modifierCalled = true
+            return image.withRenderingMode(.alwaysTemplate)
+        }
+        manager.retrieveImage(with: url, options: [.imageModifier(modifier)], progressBlock: nil) {
+            image, _, _, _ in
+            XCTAssertTrue(modifierCalled)
+            XCTAssertEqual(image?.renderingMode, .alwaysTemplate)
+            expectation.fulfill()
+        }
+        waitForExpectations(timeout: 5, handler: nil)
+    }
+
+    func testShouldApplyImageModifierWhenLoadFromMemoryCache() {
+        let expectation = self.expectation(description: "waiting for downloading and cache")
+        let URLString = testKeys[0]
+        let url = URL(string: URLString)!
+
+        var modifierCalled = false
+        let modifier = AnyImageModifier { image in
+            modifierCalled = true
+            return image.withRenderingMode(.alwaysTemplate)
+        }
+
+        manager.cache.store(testImage, forKey: URLString)
+        manager.retrieveImage(with: url, options: [.imageModifier(modifier)], progressBlock: nil) {
+            image, _, type, _ in
+            XCTAssertTrue(modifierCalled)
+            XCTAssertEqual(type, .memory)
+            XCTAssertEqual(image?.renderingMode, .alwaysTemplate)
+            expectation.fulfill()
+        }
+        waitForExpectations(timeout: 5, handler: nil)
+    }
+
+    func testShouldApplyImageModifierWhenLoadFromDiskCache() {
+        let expectation = self.expectation(description: "waiting for downloading and cache")
+        let URLString = testKeys[0]
+        let url = URL(string: URLString)!
+
+        var modifierCalled = false
+        let modifier = AnyImageModifier { image in
+            modifierCalled = true
+            return image.withRenderingMode(.alwaysTemplate)
+        }
+
+        manager.cache.store(testImage, forKey: URLString) {
+            self.manager.cache.clearMemoryCache()
+            self.manager.retrieveImage(with: url, options: [.imageModifier(modifier)], progressBlock: nil) {
+                image, _, type, _ in
+                XCTAssertTrue(modifierCalled)
+                XCTAssertEqual(type, .disk)
+                XCTAssertEqual(image?.renderingMode, .alwaysTemplate)
+                expectation.fulfill()
+            }
+        }
+        waitForExpectations(timeout: 5, handler: nil)
+    }
+#endif
 }
 
 class SimpleProcessor: ImageProcessor {
